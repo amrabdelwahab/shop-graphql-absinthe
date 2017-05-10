a52439ebd49518a62e575b319fec0d6d924db035
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-05-10 20:02:13 +0200

    Add absinthe to the dependencies

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Shop.Mixfile do
   def application do
     [mod: {Shop, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :absinthe]]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,7 +37,8 @@ defmodule Shop.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:absinthe, ">= 0.0.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
